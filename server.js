0a7c292aac8ef3acbcfc540dcf9970c464b90b39
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-11-21 09:05:04 +0600

    [DONE] docker-compose and Dockerfile

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const dbConnect = require("./db/dbConnect");
-const orderRoutes = require("./routes/order")
+const bodyParser = require("body-parser");
+const orderRoutes = require("./routes/order");
 
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-//db connect
-dbConnect().catch(err => console.log(err));
-
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
-app.use('/order', orderRoutes)
+app.use("/order", orderRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening to ${PORT}`);
-})
+  console.log(`Server is listening to ${PORT}`);
+});
